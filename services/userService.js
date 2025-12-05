2c993c5c9dc24a04a75cf00b596d9a00728dae16
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-05 22:51:01 +0100

    Amélioration de getUser
    Maintenant getUser peut obtenir 2 paramètres non obligatoire, isAdmin et isMe
    si l'un des 2 est vrai ça retournera un CompleteUser

--- services/userService.js
+++ services/userService.js
@@ -1,21 +1,25 @@
 import { getDB } from "../database/database.js";
 
-import BasicUser from '../models/userModel.js';
+import BasicUser, { CompleteUser } from '../models/userModel.js';
 
-export async function getUser(userId) {
-  try {
-      const query = `SELECT * FROM users WHERE id = ?`;
+export async function getUser(userId, isAdmin = false, isMe = false) {
+    try {
+        const query = `SELECT * FROM users WHERE id = ?`;
 
-      const db = await getDB();
-      const user = await db.get(query, [userId]);
+        const db = await getDB();
+        const user = await db.get(query, [userId]);
 
-      if (user != undefined) {
-          return new BasicUser(user.id, user.firstname, user.lastname);
-      } else {
-          return null;
-      }
-  } catch (err) {
-      console.error(err);
-      return null;
-  }
+        if (user != undefined) {
+            if (isAdmin || isMe) {
+                return new CompleteUser(user.id, user.firstname, user.lastname, user.is_admin, user.email);
+            } else {
+                return new BasicUser(user.id, user.firstname, user.lastname, user.is_admin);
+            }
+        } else {
+            return null;
+        }
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
 }
