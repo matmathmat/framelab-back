54e0df33c7b676e82805922fd1bbe1fd36c480ec
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-05 22:45:34 +0100

    Ajout de la propriété isAdmin à la classe BasicUser

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,11 @@
 import * as userService from "../services/userService.js";
 
 export default class BasicUser {
-  constructor(id, firstname, lastname) {
+  constructor(id, firstname, lastname, isAdmin) {
     this.id = id;
     this.firstname = firstname;
     this.lastname = lastname;
+    this.isAdmin = isAdmin;
   }
 
   static async getById(userId) {
@@ -17,7 +18,8 @@ export default class BasicUser {
     return new BasicUser(
       userData.id,
       userData.firstname,
-      userData.lastname
+      userData.lastname,
+      userData.isAdmin
     );
   }
 }
