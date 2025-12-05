dce5ede4ce58c06b61eb67a14f2cf63380ba90f0
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-05 09:08:53 +0100

    Ajout du template pour le back

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+// import router from "./router.js";
+// import swaggerUi from "swagger-ui-express";
+// import swaggerDocument from "../../doc/api/api.json" with { type: "json" };
+
+// Creation du serveur
+const app = express();
+app.use(express.json());
+
+// Routes API
+// app.use("/api", router);
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Exposer le dossier public
+app.use("/", express.static("public"));
+
+// Demarrage du serveur sur le port 3000
+app.listen(3000, () => {
+    console.log("Le serveur tourne à cette adresse : http://localhost:3000");
+});
