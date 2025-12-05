2852585bcc7c1fdb11f06f8a9b79d31d35ae8b09
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-05 09:28:10 +0100

    Ajout de tokenUtil
    Permet de vérifier et signer un token

---
+++ utils/tokenUtil.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+
+const apikey = 'TMVEsWCCIK9Fopuf-Beauty-Bell-Nation-Replace-Increase-Spin-Limited-Step-Ate-XawqBYSD6srIUFCl';
+
+export function signToken(data) {
+    return jwt.sign(data, apikey);
+}
+
+export function verifyToken(token) {
+    return jwt.verify(token, apikey);
+}
