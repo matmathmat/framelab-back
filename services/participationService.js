54ec7d9c852fd799854646d3213991d622eeba26
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-05 10:07:23 +0100

    Ajout du service participation
    Vérifier si une participation existe

---
+++ services/participationService.js
@@ -0,0 +1,21 @@
+import { getDB } from "../database/database.js";
+
+export async function participationExists(participationId) {
+  try {
+      const query = `
+        SELECT * FROM participations WHERE id = ?
+        `;
+
+      const db = await getDB();
+      const participation = await db.get(query, [participationId]);
+
+      if (participation != undefined) {
+          return true;
+      } else {
+          return false;
+      }
+  } catch (err) {
+      console.error(err);
+      return false;
+  }
+}
