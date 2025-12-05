ffda1d26e9272d69bf463bb7215484a43db61f65
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-05 11:00:52 +0100

    déplacement de la db dans la racine

--- database/database.js
+++ database/database.js
@@ -4,11 +4,11 @@ import { open } from 'sqlite';
 let db;
 
 export async function getDB() {
-  if (!db) {
-    db = await open({
-      filename: './framelab.db',
-      driver: sqlite3.Database
-    });
-  }
-  return db;
+    if (!db) {
+        db = await open({
+            filename: './framelab.db',
+            driver: sqlite3.Database
+        });
+    }
+    return db;
 }
