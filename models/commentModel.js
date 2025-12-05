8f9ac6651298f44e6f8f8387938b6d444ad48714
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-05 10:03:10 +0100

    Create commentModel.js

---
+++ models/commentModel.js
@@ -0,0 +1,8 @@
+export default class Comment {
+  constructor(id, textContent, commentDate, user) {
+    this.id = id;
+    this.textContent = textContent;
+    this.commentDate = commentDate;
+    this.user = user;
+  }
+}
