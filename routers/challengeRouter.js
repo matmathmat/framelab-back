4addf28d724faa0393e84cef90ea95482b92ef46
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-06 10:45:07 +0100

    Ajout middleware pour vérifier si admin

--- routers/challengeRouter.js
+++ routers/challengeRouter.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 
-import * as authController from "../controllers/authController.js";
+import { requireAdmin } from '../middlewares/authMiddleware.js';
 import { uploadMiddleware } from "../middlewares/uploadMiddleware.js";
+
+import * as authController from "../controllers/authController.js";
 import * as challengeController from "../controllers/challengeController.js";
 
 const challengeRouter = Router();
 
 challengeRouter.get("/challenges", authController.authentification, challengeController.getChallenges);
-challengeRouter.get("/challenges/:id", authController.authentification, challengeController.getChallenge);
 challengeRouter.get("/challenges/current", authController.authentification, challengeController.getCurrentChallenge);
-challengeRouter.post("/challenges", authController.authentification, uploadMiddleware, challengeController.postChallenge);
+challengeRouter.get("/challenges/:id", authController.authentification, challengeController.getChallenge);
+challengeRouter.post("/challenges", authController.authentification, requireAdmin, uploadMiddleware, challengeController.postChallenge);
 
 export default challengeRouter;
