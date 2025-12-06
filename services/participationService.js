ac762f24604508057b95639b27b6037ce860532e
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-06 01:16:02 +0100

    Ajout d'une fonction pour obtenir les participations à partir d'un challenge

--- services/participationService.js
+++ services/participationService.js
@@ -29,3 +29,41 @@ export async function getParticipation(participationId) {
     return false;
   }
 }
+
+export async function getParticipationsByChallengeId(challengeId, page = 1) {
+    try {
+        const offset = (page - 1) * 20;
+
+        const query = `
+        SELECT
+          participations.id, participations.photo_url, participations.submission_date, participations.is_visible, participations.user_id,
+          users.firstname, users.lastname
+        FROM
+            participations
+        INNER JOIN
+            users ON participations.user_id = users.id
+        WHERE
+            participations.challenge_id = ?
+        LIMIT 20
+        OFFSET ?
+        `;
+
+        const db = await getDB();
+
+        let participations = [];
+
+        await db.each(query, [challengeId, offset], (err, row) => {
+            let basicUser = new BasicUser(row.user_id, row.firstname, row.lastname, row.is_admin);
+            let participation = new Participation(row.id, row.photo_url, row.submission_date, row.is_visible, basicUser);
+            participations.push(participation);
+        });
+
+        return participations;
+    } catch (err) {
+        console.error(err);
+        return [];
+    }
+}
+
+export async function addParticipation(request, response) {
+}
