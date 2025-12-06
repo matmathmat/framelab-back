2c1bb07632015d81d898ee465f3b064db3a49f8f
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-06 11:45:53 +0100

    Pour pouvoir être déconnecté faut avoir été connecté

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import { authentification } from "../middlewares/authMiddleware.js";
+
 import { login, logout } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.get("/auth/login", login);
-authRouter.get("/auth/logout", logout);
+authRouter.get("/auth/logout", authentification, logout);
 
 export default authRouter;
