c8008a5fce5d5d3ce7c80ddbe07229f126e3260c
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-06 09:50:34 +0100

    Le router participation utilise maintenant le middleware upload

--- routers/participationRouter.js
+++ routers/participationRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import * as authController from "../controllers/authController.js";
+import { uploadMiddleware } from "../middlewares/uploadMiddleware.js";
 import * as participationController from "../controllers/participationController.js";
 
 const participationRouter = Router();
 
 participationRouter.get("/participations", authController.authentification, participationController.getParticipations);
 participationRouter.get("/participations/:id", authController.authentification, participationController.getParticipation);
-participationRouter.post("/participations", authController.authentification, participationController.postParticipation);
+participationRouter.post("/participations", authController.authentification, uploadMiddleware, participationController.postParticipation);
 
 export default participationRouter;
