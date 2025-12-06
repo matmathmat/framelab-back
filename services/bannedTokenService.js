7f597fc8df9382adc28b590445ea59d8e9c37989
matmathmat <232357120+matmathmat@users.noreply.github.com>
2025-12-06 12:34:26 +0100

    Ajout de getBannedTokenById

--- services/bannedTokenService.js
+++ services/bannedTokenService.js
@@ -27,6 +27,31 @@ export async function getBannedToken(token) {
     }
 }
 
+export async function getBannedTokenById(id) {
+    try {
+        const query = `
+        SELECT
+            id, token
+        FROM
+            banned_tokens
+        WHERE
+            id = ?
+        `;
+
+        const db = await getDB();
+        const row = await db.get(query, [id]);
+
+        if (row != undefined) {
+            return new BannedToken(row.id, row.token);
+        } else {
+            return null;
+        }
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
 export async function addBannedToken(token) {
     try {
         const db = await getDB();
@@ -45,7 +70,7 @@ export async function addBannedToken(token) {
         const insertedId = result.lastID;
 
         // On récupère les informations du token banni
-        const newBannedToken = await getBannedToken(insertedId);
+        const newBannedToken = await getBannedTokenById(insertedId);
 
         return newBannedToken;
     } catch (err) {
